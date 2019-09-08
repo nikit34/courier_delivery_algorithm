d31de85b090cb1a74c769878c451f4bad13c55f4
Никита Пермяков <38742537+nikit34@users.noreply.github.com>
2019-09-08 14:14:00 +0300

    Add files via upload

--- Untitled.py
+++ Untitled.py
@@ -5,15 +5,25 @@ from tqdm import tqdm
 import pandas as pd
 import json
 import numpy as np
+import math
 
+contest = r'contest'
+hard = r'hard'
+simple = r'simple'
 
-with open(r'contest_input.json', 'r') as read_file:
+tmp_file = contest
+
+with open(tmp_file + r'_input.json', 'r') as read_file:
     data = json.load(read_file)
 
 df_couriers = pd.DataFrame(data['couriers'])
 df_depots = pd.DataFrame(data['depots'])
 df_orders = pd.DataFrame(data['orders'])
 
+df_couriers.loc[:, 'flag_product'] = False
+
+df_depots = df_depots.rename(columns={"location_x": "pickup_location_x", "location_y":"pickup_location_y"})
+df_orders = pd.concat([df_orders, df_depots], sort=True)
 
 df_orders['delivery_time'] = 10 + np.abs(df_orders['pickup_location_x'] - df_orders['dropoff_location_x']) + np.abs(df_orders['pickup_location_y'] - df_orders['dropoff_location_y'])
 df_orders['profitable_trans'] = (df_orders['payment'] / df_orders['delivery_time'])
@@ -24,48 +34,53 @@ def distance_between_punches(loc1, loc2):
     dist = abs(loc1[0] - loc2[0]) + abs(loc1[1] - loc2[1])
     return dist
 
-min_sort_order = df_orders.sort_values('dropoff_to', axis=0, ascending=True, inplace=False)
+
+min_sort_order = df_orders.sort_values('pickup_to', axis=0, ascending=True, inplace=False)
 min_sort_order.reset_index(drop=True, inplace=True)
 tmp_start = 0
-delta_step = len(min_sort_order) // 20
-min_row_location_between_courier_order = []
-
+delta_step = math.floor(len(min_sort_order) * 0.2)
+res = pd.DataFrame(None, columns=['courier_id', 'action','order_id','point_id'])
 
 while delta_step != len(min_sort_order):
     min_tmp = float("inf")
 
-    for i in tqdm(range(tmp_start , delta_step)):
-        weight = min_sort_order['profitable_trans'][i]
+    for i in tqdm(range(tmp_start, delta_step)):
+        weight = min_sort_order.loc[i, 'profitable_trans']
+
         for j in range(len(df_couriers)):
-            distance = distance_between_punches((df_couriers['location_x'][j], min_sort_order['pickup_location_x'][i]),(df_couriers['location_y'][j], min_sort_order['pickup_location_y'][i]))
+            distance = distance_between_punches((df_couriers.loc[j, 'location_x'], min_sort_order.loc[i, 'pickup_location_x']),(df_couriers.loc[j, 'location_y'], min_sort_order.loc[i, 'pickup_location_y']))
+
             if min_tmp > distance * weight:
                 min_tmp = distance * weight
-                df_couriers['location_x'][j] = min_sort_order['pickup_location_x'][i]
-                df_couriers['location_y'][j] = min_sort_order['pickup_location_y'][i]
-                min_location_tmp_row = df_couriers.iloc[j,:]
+                df_couriers.loc[j, 'location_x'] = min_sort_order.loc[i, 'pickup_location_x']
+                df_couriers.loc[j, 'location_y'] = min_sort_order.loc[i, 'pickup_location_y']
+
+                if df_couriers.loc[j,'flag_product'] == False:
+                    res_tmp_act = 'dropoff'
+                    df_couriers.loc[j, 'flag_product'] = True
+                else:
+                    res_tmp_act = 'pickup'
+                    df_couriers.loc[j, 'flag_product'] = False
+
+                res.loc[len(res),['courier_id', 'action','order_id','point_id']] = [df_couriers.loc[j, 'courier_id'], res_tmp_act, min_sort_order.loc[i, 'order_id'], min_sort_order.loc[i, 'point_id']]
                 reset_j = j
 
-        df_couriers['location_x'][reset_j] = min_sort_order['pickup_location_x'][i]
-        df_couriers['location_y'][reset_j] = min_sort_order['pickup_location_y'][i]
+        df_couriers.loc[reset_j, 'location_x'] = min_sort_order.loc[i, 'pickup_location_x']
+        df_couriers.loc[reset_j, 'location_y'] = min_sort_order.loc[i, 'pickup_location_y']
         min_sort_order.drop([i])
 
-        i+=1
         min_tmp = float("inf")
-        min_row_location_between_courier_order.append(min_location_tmp_row)
 
-        min_sort_order.loc[i,'dropoff_location_x'] = min_sort_order['pickup_location_x'][i]
-        min_sort_order.loc[i,'dropoff_location_y'] = min_sort_order['pickup_location_y'][i]
+        min_sort_order.loc[i,'dropoff_location_x'] = min_sort_order.loc[i, 'pickup_location_x']
+        min_sort_order.loc[i,'dropoff_location_y'] = min_sort_order.loc[i, 'pickup_location_y']
 
-    tmp_start += len(min_sort_order) // 20
-    if ((len(min_sort_order) - delta_step) < len(min_sort_order)//20):
+
+    tmp_start += math.floor(len(min_sort_order) * 0.2)
+    if ((len(min_sort_order) - delta_step) <  math.ceil(len(min_sort_order) * 0.2)):
         delta_step = len(min_sort_order)
     else:
-        delta_step += len(min_sort_order) // 20
+        delta_step += math.floor(len(min_sort_order) * 0.2)
 
 
-
-
-
-res_counting = pd.DataFrame(min_row_location_between_courier_order)
-
-print(res_counting)
+res.to_json(tmp_file + r'_output.json', orient='records')
+print(res)
